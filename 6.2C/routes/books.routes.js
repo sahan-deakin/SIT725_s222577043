a54fae522191197e5013388a66a1a4a243bbc062
Sahan Thiranjaya Devage Don <s222577043@deakin.edu.au>
2026-05-06 02:19:31 +1000

    5.4D:test-update

--- 6.2C/routes/books.routes.js
+++ 6.2C/routes/books.routes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 
 const Controllers = require('../controllers');
 
-// Route to get all books
 router.get('/', Controllers.booksController.getAllBooks);
-
-// Route to get a book by its ID
 router.get('/:id', Controllers.booksController.getBookById);
 
-module.exports = router; 
+module.exports = router;
